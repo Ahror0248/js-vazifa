c586664d865fceb8d991f3de53069b44d1df4981
Ahror0248 <akhrorcodrr@gmail.com>
2021-06-21 10:45:04 +0500

    successful

--- index.js
+++ index.js
@@ -63,15 +63,97 @@ const p = 3.14
 // console.log(a+b+c)
 
     // Begin-13 
-    var R1=5
-    var R2=4
-    var S1 = p*R1
-    var S2 = p*R2
-    var S3 = p*(R1-R2)
-    console.log(S1, S2, S3 )
+    // var R1=5
+    // var R2=4
+    // var S1 = p*R1
+    // var S2 = p*R2
+    // var S3 = p*(R1-R2)
+    // console.log(S1, S2, S3 )
 
 // Begin-14 
 // var L=5
 // var R = L/2*p
 // var S=p*R**2
 // console.log(R)
+
+// 15
+// var S = 5
+//     r = S/(2*p)
+//     d = p*r**2
+// console.log(d)
+
+// 16
+// var x2 = 4
+//     x1 = 7
+// console.log(Math.abs(x2-x1))
+
+// 17
+// var A = 2
+//     B = 3
+//     C = 4
+//     AC = A+C
+//     BC = B+C
+// console.log(AC, BC)
+
+// 18
+// var A = 4
+//     C = 6
+//     B = 9
+//     AC = A*C
+//     BC = B*C
+// console.log(AC, BC)
+
+// 19
+// var a = 4
+//     b = 5
+//     P = 2*(a+b)
+//     S = a*b
+// console.log(P, S)
+
+// 20
+// var x1 = 6
+//     y1 = 5
+//     x2 = 4
+//     y2 = 3
+// console.log(Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2)))
+
+// 21
+
+// console.log(Math.sqrt(25))
+// console.log(Math.round(8.3, 8,9))
+// console.log(Math.round(8.5))
+// console.log(Math.abs(-11))
+// console.log(Math.ceil(4.2))
+// console.log(Math.ceil(4.000000001))
+// console.log(Math.ceil(4.000000001))
+// console.log(Math.floor(4.99999))
+// console.log(Math.max(1,3,4,5,6,7,9,0))
+// console.log(Math.min(1,3,4,5,6,7,9,0))
+// console.log(Number.parseInt (Math.random()*100))
+
+// // 21
+// var a = 3
+//     b = 4
+//     c = 5
+//     P = (a+b+c)/2
+// S = Math.sqrt(P*(P-a)*(P-b)*(P-c))
+// console.log(S)
+
+// 22
+// var a=2
+//     b=4
+//     c=a
+// a=b
+// b=c
+// console.log(a,b)
+
+// 23 
+let a=2
+let b=3
+let c=4
+    
+var d=a //2
+a=b //3
+b=c
+c=d
+console.log(a, b, c)    
